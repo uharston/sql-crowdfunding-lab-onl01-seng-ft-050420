e9d89ee2722f4b7aa6794592a480f97d94cadefc
Uriah Harston <uharston@gmail.com>
2020-06-02 14:21:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   FROM users
   INNER JOIN pledges
   ON users.id = pledges.user_id
-  GROUP BY users.name 
+  GROUP BY users.name
   ORDER BY pledges.amount"
 end
 
