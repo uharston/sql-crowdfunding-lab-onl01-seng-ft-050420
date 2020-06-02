bb58111a718ea7b9dc84e09235fb8236f3ee18d9
Uriah Harston <uharston@gmail.com>
2020-06-02 14:21:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,9 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.names, SUM(pledges.amount)
   FROM users
   INNER JOIN pledges
-  ON users.id = pledges.user"
+  ON users.id = pledges.user_id
+  GROUP BY users.name 
+  ORDER BY pledges.amount"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
