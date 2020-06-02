71347d0874051096a0db0a6782bcfe4cb391c4de
Uriah Harston <uharston@gmail.com>
2020-06-02 14:10:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,9 +25,9 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
   FROM projects
-  INNER JOIN pledges 
+  INNER JOIN pledges
   ON projects.id = pledges.user_id
-  GROUP BY projects.title 
+  GROUP BY projects.title
   "
 end
 
